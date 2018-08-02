4101f773f57d3098a617bcff90298ed8fedc652d
Fabian Casilimas <fabiancasilimas@gmail.com>
2018-08-02 16:36:41 -0500

    Autenticación de usuarios y administrador con Devise.

--- appzona/config/routes.rb
+++ appzona/config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
+  resources :posts
+  devise_for :users
+  get 'home/index'
+  get 'web/index'
   match '/contacts',	to: 'contacts#new',	via: 'get'	
   resources :contacts,	only: [:new, :create]
   #resources :contacts, except: [:new, :create]
   #resources :contacts
   #get 'contacts/new'
   #get 'contacts/create'
-  resources :users
-  get 'web/index'
+
+  #root to: 'home#index'
+  #root :to => 'home#index'
   root :to => 'web#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
